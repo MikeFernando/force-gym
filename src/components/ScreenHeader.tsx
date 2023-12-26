c98bab81f118ef3d5f14e73bc347c25a076d5408
Mike Fernando <mikera2020@outlook.com>
2023-12-26 20:28:06 -0300

    component HistoryCard

---
+++ src/components/ScreenHeader.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native"
+
+export type Props = {
+  title: string
+}
+
+export function ScreenHeader({ title }: Props) {
+  return (
+    <View className="bg-GRAY_600 pt-16 px-8 pb-6 items-center justify-center">
+      <Text className="text-GRAY_100 text-lg text-center font-semibold">
+        {title}
+      </Text>
+    </View>
+  )
+}
