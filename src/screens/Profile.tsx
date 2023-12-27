0a80206820c383678e135b1aad9283548339dae3
Mike Fernando <mikera2020@outlook.com>
2023-12-27 13:59:33 -0300

    UserPhoto perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,22 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
 
 export function Profile() {
   return (
-      <View className="flex-1 justify-center items-center">
-        <Text className="text-white">
-          Profile
-        </Text>
+      <View className="flex-1">
+        <ScreenHeader title="Perfil" />
+
+        <ScrollView>
+          <View className="items-center justify-center mt-8">
+            <UserPhoto 
+              source={{ uri: "https://github.com/MikeFernando.png" }}
+              alt="Foto de perfil"
+              size={128}
+            />
+          </View>
+        </ScrollView>
       </View>
   )
 }
